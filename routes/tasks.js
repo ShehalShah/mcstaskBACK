8c4745c5840e7eced2b8677891f7a23e9333d9db
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2023-06-14 17:07:35 +0530

    model

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/task');
+const Task = require('../Models/task');
 
 router.post('/tasks', async (req, res) => {
   try {
